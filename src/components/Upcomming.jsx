2752efe03e5aa08e462dd94e0b17daa869485451
Mahidi <mahidishikder@Mahidis-MacBook-Air.local>
2025-07-13 14:24:21 +0600

    red

--- src/components/Upcomming.jsx
+++ src/components/Upcomming.jsx
@@ -51,7 +51,7 @@ const UpcomingDeadlines = () => {
   return (
     <section className="max-w-7xl mx-auto px-4 py-12">
       <h2 className="text-3xl font-bold text-center text-[#3DB371] mb-10">
-        📅 Upcoming Admission Deadlines
+        📅 Upcoming Admission Deadline
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
